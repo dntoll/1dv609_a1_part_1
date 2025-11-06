f0352d5b218dbbbe5baae7104f87b9a07bcb2f71
Daniel Toll <ttoda@lnu.se>
2025-11-06 10:54:32 +0100

    js tests

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,8 +1,20 @@
-const Password = require('../src/sut_versions/Correct'); // Adjust the path as necessary
+//const Password = require('../src/sut_versions/BugDoesNotHash');
+//const Password = require('../src/sut_versions/BugDoesNotTrim');
+//const Password = require('../src/sut_versions/BugisPasswordAlwaysSame'); 
+//const Password = require('../src/sut_versions/BugMissingNumberCheck'); 
+//const Password = require('../src/sut_versions/BugMissingPasswordCheck');
+//const Password = require('../src/sut_versions/BugNeverContainsNumbers');
+//const Password = require('../src/sut_versions/BugToShortPassword'); 
+//const Password = require('../src/sut_versions/BugVeryShort'); 
+//const Password = require('../src/sut_versions/BugWrongHashingAlgorithm'); 
+//const Password = require('../src/sut_versions/BugWrongMessage'); 
+const Password = require('../src/sut_versions/Correct'); 
 
 describe('Password', () => {
     test('placeholder', () => {
+
         expect(true).toBe(true);
     });
 
+    //Add your tests here
 });
